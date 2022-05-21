26a3c5504e72bb6bf76ad4ab29ba681b561bc6f9
Evan Murray <evan@auraaudio.io>
2022-05-21 10:02:45 -0500

    Initial Commit

---
+++ Shared/OscillatorBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  OscillatorBankApp.swift
+//  Shared
+//
+//  Created by Aura Audio on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct OscillatorBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
