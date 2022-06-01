471850024b8b7dec783d1ded35274e67e1af67d5
emurray2 <evan@auraaudio.io>
2022-06-01 16:42:17 -0500

    Get rid of MacOS stuff and Tests

--- OscillatorBank-AU-macOS/OscillatorBank_AU_macOS-Bridging-Header.h
+++
@@ -1,4 +0,0 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-
-#import "OscillatorBank_AU_macOSDSPKernelAdapter.h"
-#import "OscillatorBank_AU_macOSAudioUnit.h"
