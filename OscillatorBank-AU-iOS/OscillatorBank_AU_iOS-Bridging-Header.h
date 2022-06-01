522426b45b7cd8716a8179efef77d28eea527c47
emurray2 <evan@auraaudio.io>
2022-06-01 18:44:06 -0500

    Remove Bridging Header - Use Framework

--- OscillatorBank-AU-iOS/OscillatorBank_AU_iOS-Bridging-Header.h
+++
@@ -1,4 +0,0 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-
-#import "OscillatorBank_AU_iOSDSPKernelAdapter.h"
-#import "OscillatorBank_AU_iOSAudioUnit.h"
--- OscillatorBank-AU-iOS/UI/AudioUnitViewController.swift
+++ OscillatorBank-AU-iOS/UI/AudioUnitViewController.swift
@@ -1,6 +1,7 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 import CoreAudioKit
+import OscillatorBankFramework
 
 public class AudioUnitViewController: AUViewController, AUAudioUnitFactory {
     var audioUnit: AUAudioUnit?
