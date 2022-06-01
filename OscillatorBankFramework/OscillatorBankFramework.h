12d4f866b4682b71dd0d484c56880ba04933f29e
emurray2 <evan@auraaudio.io>
2022-06-01 17:22:33 -0500

    Add plugin framework

---
+++ OscillatorBankFramework/OscillatorBankFramework.h
@@ -0,0 +1,19 @@
+//
+//  OscillatorBankFramework.h
+//  OscillatorBankFramework
+//
+//  Created by Evan Murray on 6/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OscillatorBankFramework.
+FOUNDATION_EXPORT double OscillatorBankFrameworkVersionNumber;
+
+//! Project version string for OscillatorBankFramework.
+FOUNDATION_EXPORT const unsigned char OscillatorBankFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OscillatorBankFramework/PublicHeader.h>
+#import <OscillatorBankFramework/OscillatorBank_AU_iOSDSPKernelAdapter.h>
+#import <OscillatorBankFramework/OscillatorBank_AU_iOSAudioUnit.h>
+
